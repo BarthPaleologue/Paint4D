4b7e411ec4a6b0a5ffb6e5c408377bbe24e819e2
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2022-10-23 20:18:49 +0200

    added icons to shape menu

--- shapemenu.cpp
+++ shapemenu.cpp
@@ -4,11 +4,11 @@
 ShapeMenu::ShapeMenu(QWidget *parent): QMenu(tr("Shape"), parent)
 {
     group = new QActionGroup(this);
-    line = new QAction(tr("&line"), this);
+    line = new QAction(QIcon(":/icons/line.png"), tr("&line"), this);
     line->setData(QVariant(static_cast<int>(ShapeEnum::LINE)));
-    rectangle = new QAction(tr("&rectangle"), this);
+    rectangle = new QAction(QIcon(":/icons/rectangle.png"), tr("&rectangle"), this);
     rectangle->setData(QVariant(static_cast<int>(ShapeEnum::RECTANGLE)));
-    ellipse = new QAction(tr("&ellipse"), this);
+    ellipse = new QAction(QIcon(":/icons/ellipse.png"), tr("&ellipse"), this);
     ellipse->setData(QVariant(static_cast<int>(ShapeEnum::ELLIPSE)));
 
     group->addAction(line);
